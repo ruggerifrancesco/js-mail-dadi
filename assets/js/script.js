e7ef8d54f5b3afda4fed647d62fe5957606373da
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-17 16:39:31 +0200

    cHNAGED PSOTION AND ADDED CONLE LOG

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,28 @@
 console.log('JS Loaded Succesfully');
 
-// User Input
-const userValue = parseInt(document.getElementById('userValueInput').value);
-    console.log('Output Test userValue = ' + userValue);
-const submitBtn = document.getElementById('submitBtn');
-
 // Dice Section
 
+    // User Input
+    const userValue = parseInt(document.getElementById('userValueInput').value);
+        console.log('Output Test userValue = ' + userValue);
+
     // PC random Number
-    let randomNumber = Math.floor(Math.random() * 10) + 1;
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
     const randomPcOutput = document.getElementById('computerValueOutput');
 
         if (randomNumber >= 1 && randomNumber <= 6) { 
-        //  console.log(`The random number ${randomNumber} is between 1 and 6`);
+            console.log(`The random number ${randomNumber} is between 1 and 6`);
             randomPcOutput.innerHTML = randomNumber;
         }
+
+    // Button Submit 
+    const submitBtn = document.getElementById('submitBtn');
+    
+    submitBtn.addEventListener("click",
+        function () {
+
+        }
+    )
 
 
 
