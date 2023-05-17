c91a8f117fd83b49bc5081aeb974c7ed37cb5025
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-17 16:17:52 +0200

    Added ifrst number randomizer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,29 @@
 console.log('JS Loaded Succesfully');
 
-// Testing Random Number Output
-let randomNumber = Math.round(Math.random() * 6);
-    console.log('Test Method Random Number = ' + randomNumber);
-
 // Dice Section
 for (let i = 1; i <= 6; i++) {
     // console.log(i);
 
     // User Input
-    const userValue = parseInt(document.getElementById(userValueInput).value);
+    const userValue = parseInt(document.getElementById('userValueInput').value);
+        console.log('Output Test userValue = ' + userValue);
+    const submitBtn = document.getElementById('submitBtn');
 
-    if () {
+    submitBtn.addEventListener ('click', 
+        function() {
 
-    }
+        }
+    )
+
+    // PC random Number
+    let random_number = Math.floor(Math.random() * 10) + 1;
+        if (random_number >= 1 && random_number <= 6) { 
+          console.log(`The random number ${random_number} is between 1 and 6`);
+        } else {
+          console.log(`The random number ${random_number} is not between 1 and 6`);
+        }
 }
+
 
 
 // Mail Section
