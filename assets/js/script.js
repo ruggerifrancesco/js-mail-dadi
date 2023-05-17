c254e1ee113f7a47f8c9ce3f192c5a4e8af2590f
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-17 16:54:23 +0200

    chnaged strcutrue to add in eventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,30 @@
 console.log('JS Loaded Succesfully');
 
 // Dice Section
-
-    // User Random Number
-    let randomUserNumber = Math.floor(Math.random() * 6) + 1;
-    const randomUserOutput = document.getElementById('userValueOutput');
-
-        if (randomUserNumber >= 1 && randomUserNumber <= 6) { 
-            console.log(`Your random number ${randomUserNumber} is between 1 and 6`);
-            randomUserOutput.innerHTML = randomUserNumber;
-        }
-
-
-    // PC Random Number
-    let randomPcNumber = Math.floor(Math.random() * 6) + 1;
-    const randomPcOutput = document.getElementById('computerValueOutput');
-
-        if (randomPcNumber >= 1 && randomPcNumber <= 6) { 
-            console.log(`PC random number ${randomPcNumber} is between 1 and 6`);
-            randomPcOutput.innerHTML = randomPcNumber;
-        }
-
     // Button Submit 
     const submitBtn = document.getElementById('submitBtn');
     
     submitBtn.addEventListener("click",
         function () {
+
+            // User Random Number
+            let randomUserNumber = Math.floor(Math.random() * 6) + 1;
+            const randomUserOutput = document.getElementById('userValueOutput');
+
+                if (randomUserNumber >= 1 && randomUserNumber <= 6) { 
+                    console.log(`Your random number ${randomUserNumber} is between 1 and 6`);
+                    randomUserOutput.innerHTML = randomUserNumber;
+                }
+
+            // PC Random Number
+            let randomPcNumber = Math.floor(Math.random() * 6) + 1;
+            const randomPcOutput = document.getElementById('computerValueOutput');
+            
+                if (randomPcNumber >= 1 && randomPcNumber <= 6) { 
+                    console.log(`PC random number ${randomPcNumber} is between 1 and 6`);
+                    randomPcOutput.innerHTML = randomPcNumber;
+                }
+
         }
     )
 
